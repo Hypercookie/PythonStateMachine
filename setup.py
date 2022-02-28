635f454a60cb86431d557c5ca7ef0d60d61c4cee
Jannes Müller <jannes.mueller@uni-konstanz.de>
2022-02-28 16:13:51 +0100

    Ref

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup
 
 setup(
     name="hyper_machines",
-    version="0.0.2",
-    packages=["hyper_machines"],
+    version="0.0.3",
+    packages=["edge_machine", "state_machine"],
     url="https://github.com/Hypercookie/PythonStateMachine",
     license="MIT",
     author="hypercookie",
     author_email="hypercookie@gmx.de",
     description="Machines for Python",
-    download_url = "https://github.com/Hypercookie/PythonStateMachine/archive/refs/tags/0.0.2.tar.gz",
+    download_url="https://github.com/Hypercookie/PythonStateMachine/archive/refs/tags/0.0.3.tar.gz",
 
 )
-
